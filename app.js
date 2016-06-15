fd402e772f5f1be9b9324e400748689705da3498
Santiago Martin <santiagomartinagra@gmail.com>
2016-06-15 20:26:46 +0200

    Added config file

--- app.js
+++ app.js
@@ -3,33 +3,48 @@ var speedTest = require('speedtest-net'),
     Datastore = require('nedb'),
     db = new Datastore({ filename: 'data/db.db', autoload: true }),
     express = require('express'),
-    app = express();
+    app = express(),
+    config = require("./config.json");
 
 
 var internetTest = function() {
     speedTest.visual({maxTime: 5000}, function(err, data) {
-        console.dir(data.speeds);
 
-        db.insert({
+        if(data){
+            console.log("Hay internet")
+            db.insert({
 
-            date : new Date(),
-            download : data.speeds.download,
-            upload : data.speeds.upload,
-            host : data.server.host,
-            sponsor : data.server.sponsor
+                date : new Date(),
+                download : data.speeds.download,
+                upload : data.speeds.upload,
+                host : data.server.host,
+                sponsor : data.server.sponsor
 
-        });
+            });
+        }
+        else{
+            console.log("No hay internet")
+            db.insert({
+
+                date : new Date(),
+                download : "No internet connection",
+                upload : "No internet connection",
+                host : "No internet connection",
+                sponsor : "No internet connection"
+
+            });
+        }
+
     });
 };
 
 try{
-    var job = new CronJob('* */5 * * * *', function() {
-        console.log("Start");
+    var job = new CronJob(config.crontab, function() {
         internetTest();
     });
     job.start();
 } catch (ex) {
-    console.log("Expresion error")
+    console.log("Crontab expresion error")
 }
 
 
@@ -43,6 +58,6 @@ app.get('/', function(req, res){
 });
 
 app.set('view engine', 'pug');
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(config.port, function () {
+    console.log('Internet Status Checker running on port ' + config.port + '!');
 });
